37c3a9208cd41b1416cd7d712cae2dbbaf58befd
Ruyan Chen <colortheskyjade@users.noreply.github.com>
2023-01-12 11:52:00 -0800

    need to symlink manually

--- setup.sh
+++ setup.sh
@@ -4,3 +4,6 @@ set -o errexit  # Exit if a command fails
 set -o nounset  # Exit when using an undeclared variable
 set -o pipefail # Catch pipe failures
 
+ln -s .bash_aliases $HOME/.bash_aliases
+ln -s .tmux.conf $HOME/.tmux.conf
+tmux source-file $HOME/.tmux.conf
