17b2e5201e212cf568e7c4db102bf19c7d00e17f
Ruyan Chen <colortheskyjade@users.noreply.github.com>
2023-01-12 11:58:51 -0800

    incorrect dir

--- setup.sh
+++ setup.sh
@@ -4,6 +4,8 @@ set -o errexit  # Exit if a command fails
 set -o nounset  # Exit when using an undeclared variable
 set -o pipefail # Catch pipe failures
 
-ln -s .bash_aliases $HOME/.bash_aliases
-ln -s .tmux.conf $HOME/.tmux.conf
+scriptDir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
+ln -s $scriptDir/.bash_aliases $HOME/.bash_aliases
+ln -s $scriptDir/.tmux.conf $HOME/.tmux.conf
 tmux source-file $HOME/.tmux.conf
